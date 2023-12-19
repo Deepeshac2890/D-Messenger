de6f5e81fd5665ff24bc37a7171808ff080a7e9f
Deepeshac2890 <deepeshac290@gmail.com>
2023-12-19 19:34:16 +0530

    Bootstrap added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+  
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -8,5 +10,55 @@ class ArticlesController < ApplicationController
         # return render json: {
         #     success: true
         # }
+    end
+
+    def home
+    end
+
+    def new
+      @article = Article.new
+    end
+
+    def edit
+    end
+
+    def create
+      @article = Article.new(article_params)
+      @res = @article.save
+      if (@res)
+        flash[:notice]  = "Article was created successfully."
+        redirect_to article_path(@article)
+      else
+        render 'new'
+      end
+    end
+
+    def update
+      @res = @article.update(article_params)
+      if (@res)
+        flash[:notice]  = "Article was updated successfully."
+        redirect_to article_path(@article)
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @res = @article.destroy
+      if (@res)
+        flash[:notice]  = "Article was deleted successfully."
+        redirect_to articles_path
+      else
+        flash[:notice]  = "Article was not deleted successfully."
+      end
+    end
+
+    private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index]
+  root 'articles#home'
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
