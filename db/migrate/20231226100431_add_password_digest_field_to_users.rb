0f83ee4023e7f6e542332e40ac59e14f5c5d6d9a
Deepeshac2890 <deepeshac290@gmail.com>
2023-12-26 16:10:41 +0530

    Added the secure password to user

---
+++ db/migrate/20231226100431_add_password_digest_field_to_users.rb
@@ -0,0 +1,6 @@
+class AddPasswordDigestFieldToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :password_digest, :string
+    remove_column :users, :password
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_26_084622) do
+ActiveRecord::Schema.define(version: 2023_12_26_100431) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 2023_12_26_084622) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "password_digest"
   end
 
 end
