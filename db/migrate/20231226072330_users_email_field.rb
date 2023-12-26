7342cee57ea66e6d3ad2704a6e3eb03c7ad1e4e1
Deepeshac2890 <deepeshac290@gmail.com>
2023-12-26 14:04:38 +0530

    Added User Validations

---
+++ db/migrate/20231226072330_users_email_field.rb
@@ -0,0 +1,5 @@
+class UsersEmailField < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_26_071015) do
+ActiveRecord::Schema.define(version: 2023_12_26_072330) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2023_12_26_071015) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
