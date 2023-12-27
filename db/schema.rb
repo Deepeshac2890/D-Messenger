24a233848f29110f9fb24140c65dbda1d548e44e
Deepeshac2890 <deepeshac290@gmail.com>
2023-12-27 10:52:51 +0530

    Add users profile show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_26_100431) do
+ActiveRecord::Schema.define(version: 2023_12_27_045842) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2023_12_26_100431) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_digest"
+    t.string "profile_image"
   end
 
 end
